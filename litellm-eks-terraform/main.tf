0eb00112983bdfc077b5abda049b8947df6e35b8
michael rodriguez <mirodrr@amazon.com>
2025-02-05 20:41:29 +0100

    WIP

--- litellm-eks-terraform/main.tf
+++ litellm-eks-terraform/main.tf
@@ -63,12 +63,12 @@ resource "aws_cloudformation_stack" "guidance_deployment_metrics" {
 }
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+  host                   = local.cluster_endpoint
+  cluster_ca_certificate = base64decode(local.cluster_ca)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
   }
 }
 
